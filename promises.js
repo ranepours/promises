fa1eb9b0dae9b5b28ee7878f53ececfaeac55b7c
Rane Jasia <ranedavis@ranepours.attlocal.net>
2022-08-27 19:16:35 -0400

    CHRISSY WAKE UP! I DONT LIKE THIS

--- promises.js
+++ promises.js
@@ -1,48 +1,10 @@
 const axios = require('axios');
-//NUMBER FACTS
-//get fact about favorite number
-const someFact = (num) => {
-    return somePromise = axios.get(`http://numbersapi.com/${num}`)
-        .then(res => console.log(res.data))
-        .catch(err => console.log("rejected", err))
-}
-someFact(7);
-//get data on multiple numbers in single request and put all of them on page
-let fourNumPromises = [];
-for(let i = 1; i < 5; i++){
-    fourNumPromises.push(axios.get(`http://numbersapi.com/${i}`));
-}
-Promise.all(fourNumPromises)
-    .then(numArr => {
-        for(res of numArr){
-            console.log(res.data);
-        }
-    })
-    .catch(err => console.log(err));
-
-//get 4 facts on favorite number and put them on page
-let url = "http://numbersapi.com/3";
-let Promise1 = axios.get(url)
-    .then(res => {
-        console.log(res.data);
-    })
-    .catch(err => console.log("rejected", err));
-let Promise2 = axios.get(url)
-    .then(res => {
-        console.log(res.data);
-    })
-    .catch(err => console.log("rejected", err));
-let Promise3 = axios.get(url)
-    .then(res => {
-        console.log(res.data);
-    })
-    .catch(err => console.log("rejected", err));
-let Promise4 = axios.get(url)
-    .then(res => {
-        console.log(res.data);
-    })
-    .catch(err => console.log("rejected", err));
-
-
 //DECK OF CARDS
- 
+let baseURL = "https://deckofcardsapi.com/api/deck";
+//make request to api from newly shuffled deck and log result to console
+const getCard = () => {
+    
+}
+//make request to get a card and another card after; log results from both to console
+
+//build html page which lets u draw from deck and render card to page each time u click "get card" button until all 52 cards have loaded onto page
