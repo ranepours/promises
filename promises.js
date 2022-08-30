c7b38d15dba96f71b156d7b9ebad03e29df5be6a
Rane Jasia <ranedavis@ranepours.attlocal.net>
2022-08-29 20:46:30 -0400

    ITS KINDA WORKING

--- promises.js
+++ promises.js
@@ -1,34 +1,43 @@
-const axios = require('axios');
-//DECK OF CARDS
 let baseURL = "https://deckofcardsapi.com/api/deck";
-//make request to api from newly shuffled deck and log result to console
-const getCard = () => {
-    return Promise = axios.get(`${baseURL}/new/draw/`)
-        .then(res => {
-            let { suit, value } = res.cards[0];
-            console.log(`${value} of ${suit}`);
-        }).catch(err => {
-            console.log("rejected", err)
-        })
-}
-getCard();
+// //get A card and log its value
+let getCard = axios.get(`${baseURL}/new/draw/`).then(res => {
+    let value = res.data.cards[0].value;
+    let suit = res.data.cards[0].suit;
+    //console.log(res.data.cards[0]);
+    console.log(`${value} of ${suit}`);
+})
 
-//make request to get a card and another card after; log results from both to console
-let first = null;
-const getCards = () => {
-    let Promise = axios.get(`${baseURL}/new/draw/`).then(res => {
-        first = data.cards[0];
-        let deckId = data.deck_id;
-        return axios.get(`${baseURL}/${deckId}/draw`);
-    }).then(res => {
-        let second = data.cards[0];
-        [first, second].forEach(function(card){
-            console.log(`${card.value} of ${card.suit}`)
-        })
-    }).catch(err => {
-        console.log("rejected", err);
+// //make two requests FROM THE SAME DECK so we get two separate cards and log their values
+let cardOne;
+let getCards = axios.get(`${baseURL}/new/draw/`).then(res => {
+    cardOne = res.data.cards[0];
+    let deckID = res.data.deck_id;
+    return axios.get(`${baseURL}/${deckID}/draw/`)
+}).then(res => {
+    let cardTwo = res.data.cards[0];
+    [cardOne, cardTwo].forEach((card) => {
+        console.log(`${card.value} of ${card.suit}`)
     })
-}
-getCards();
+})
 
 //build html page which lets u draw from deck and render card to page each time u click "get card" button until all 52 cards have loaded onto page
+let deckID;
+let button = document.querySelector("button");
+let cardSpace = document.getElementById("#card-space");
+
+let deck = axios.get(`${baseURL}/new/shuffle`).then(res => {
+    deckID = res.data.deck_id;
+})
+button.addEventListener("click", () => {
+    render = axios.get(`${baseURL}/${deckID}/draw/`).then(res => {
+        let imgSrc = res.data.cards[0].image;
+        let angle = Math.random() * 90 - 45;
+        let xAxis = Math.random() * 40 - 20;
+        let yAxis = Math.random() * 40 - 20;
+
+        let card = document.createElement("img");
+        card.src = imgSrc;
+        console.log(card.src);
+        if (res.data.remaining === 0) button.remove()
+    })
+})
