92f12490b682d5de43a4836316bfcb28b990664a
Rane Jasia <ranedavis@ranepours.attlocal.net>
2022-08-30 18:34:27 -0400

    I FIXED ITgit add .git add .git add .git add .git add .git add . IM A GENUISgit add .git add .git add .git add .git add .git add .git add .

---
+++ cards/app.js
@@ -0,0 +1,75 @@
+//recreate axios bc for some reason its not working any way i call it
+function get(url){
+    const request = new XMLHttpRequest();
+    return new Promise ((resolve,reject) => {
+        request.onload = function(){
+            if(request.readyState != 4) return;
+
+            //check status code
+            if(request.status >= 200 && request.status < 300){
+                resolve({
+                    data: JSON.parse(request.response),
+                    status: request.status,
+                    request: request
+                })
+            } else {
+                reject({
+                    msg: "server error",
+                    status: request.status,
+                    request: request
+                })
+            }
+        }
+        request.onerror = function handleError(){
+            request = null;
+            reject({msg: "network error"});
+        }
+        request.open("GET", url);
+        request.send();
+    })
+}
+
+let baseURL = "https://deckofcardsapi.com/api/deck";
+
+// //get A card and log its value
+let getCard = get(`${baseURL}/new/draw/`).then(res => {
+    let value = res.data.cards[0].value;
+    let suit = res.data.cards[0].suit;
+    //console.log(res.data.cards[0]);
+    console.log(`${value} of ${suit}`);
+})
+
+// //make two requests FROM THE SAME DECK so we get two separate cards and log their values
+let cardOne;
+let getCards = get(`${baseURL}/new/draw/`).then(res => {
+    cardOne = res.data.cards[0];
+    let deckID = res.data.deck_id;
+    return get(`${baseURL}/${deckID}/draw/`)
+}).then(res => {
+    let cardTwo = res.data.cards[0];
+    [cardOne, cardTwo].forEach((card) => {
+        console.log(`${card.value} of ${card.suit}`)
+    })
+})
+
+//build html page which lets u draw from deck and render card to page each time u click "get card" button until all 52 cards have loaded onto page
+let deckID;
+const BTN = document.querySelector("button");
+const cardSpace = document.querySelector("#card-space");
+
+let deck = get(`${baseURL}/new/shuffle`).then(res => {
+    return deckID = res.data.deck_id;
+})
+BTN.addEventListener("click", () => {
+    render = get(`${baseURL}/${deckID}/draw/`).then(res => {
+        imgSrc = res.data.cards[0].image;
+        let card = document.createElement("img");
+        card.src = imgSrc;
+        console.log(card.src)
+        cardSpace.append(card);
+        if (res.data.remaining === 0) {
+            button.remove();
+            alert("All cards pulled!");
+        }
+    })
+})
--- numberFacts.js
+++ numberFacts.js
@@ -1,8 +1,37 @@
-const axios = require('axios');
+//recreate axios bc for some reason its not working any way i call it
+function get(url){
+    const request = new XMLHttpRequest();
+    return new Promise ((resolve,reject) => {
+        request.onload = function(){
+            if(request.readyState != 4) return;
+
+            //check status code
+            if(request.status >= 200 && request.status < 300){
+                resolve({
+                    data: JSON.parse(request.response),
+                    status: request.status,
+                    request: request
+                })
+            } else {
+                reject({
+                    msg: "server error",
+                    status: request.status,
+                    request: request
+                })
+            }
+        }
+        request.onerror = function handleError(){
+            request = null;
+            reject({msg: "network error"});
+        }
+        request.open("GET", url);
+        request.send();
+    })
+}
 //NUMBER FACTS
 //get fact about favorite number
 const someFact = (num) => {
-    return somePromise = axios.get(`http://numbersapi.com/${num}`)
+    return somePromise = get(`http://numbersapi.com/${num}`)
         .then(res => console.log(res.data))
         .catch(err => console.log("rejected", err))
 }
@@ -10,7 +39,7 @@ someFact(7);
 //get data on multiple numbers in single request and put all of them on page
 let fourNumPromises = [];
 for(let i = 1; i < 5; i++){
-    fourNumPromises.push(axios.get(`http://numbersapi.com/${i}`));
+    fourNumPromises.push(get(`http://numbersapi.com/${i}`));
 }
 Promise.all(fourNumPromises)
     .then(numArr => {
@@ -20,24 +49,24 @@ Promise.all(fourNumPromises)
     })
     .catch(err => console.log(err));
 
-//get 4 facts on favorite number and put them on page
+//get 4 facts on favorite number
 let url = "http://numbersapi.com/3";
-let Promise1 = axios.get(url)
+let Promise1 = get(url)
     .then(res => {
         console.log(res.data);
     })
     .catch(err => console.log("rejected", err));
-let Promise2 = axios.get(url)
+let Promise2 = get(url)
     .then(res => {
         console.log(res.data);
     })
     .catch(err => console.log("rejected", err));
-let Promise3 = axios.get(url)
+let Promise3 = get(url)
     .then(res => {
         console.log(res.data);
     })
     .catch(err => console.log("rejected", err));
-let Promise4 = axios.get(url)
+let Promise4 = get(url)
     .then(res => {
         console.log(res.data);
     })
